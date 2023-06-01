66f0e8cb7055e63d4edc475de617b1d2ae759321
bound <boundwhd@126.com>
2023-06-01 17:16:11 +0800

    v-6.1.2

--- tb.c
+++ tb.c
@@ -1,13 +1,6 @@
 #include "3DNR.h"
 
 int main() {
-	FILE* f = fopen("D:\\RAWtest\\AFTER_DENOISE\\IMG_7_2_after_denoise.raw","rb");
-    char filename[] = "IMG_7_2_after_denoise.raw";
-    if (!f) {
-        printf("Err: load_image: file not found! (%s)\n", filename);
-        (-1);
-    }
-    else {
-        printf("(%s) successfully open!\n",filename);
-    }
+	printf("hello.c\n");
 }
+
